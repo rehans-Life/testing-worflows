21ed85a88c0d7360dfa94856e1b10331e8c31112
Your Name <you@example.com>
2023-12-24 02:46:02 -0800

    heheh

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const app = express();
 app.use(express.static("public"));
 
 app.listen(process.env.PORT, () =>
-  console.log("Server listening on PORT 4000")
+  console.log(`Server listening on PORT ${process.env.PORT}`)
 );
