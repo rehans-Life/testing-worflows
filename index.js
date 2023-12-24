5607150ad29eeb4a93c015d71625b2e8bc2b03da
Your Name <you@example.com>
2023-12-24 02:39:08 -0800

    first workflow

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(process.env.PORT, () =>
+  console.log("Server listening on PORT 4000")
+);
